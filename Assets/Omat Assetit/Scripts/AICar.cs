7a1e42c4ec8b279bdb91ece0f4d453ca09110821
Leevi Tulijoki <leevi.tulijoki@outlook.com>
2025-10-03 18:15:39 +0300

    lisätty vastustajan liikkuminen

--- Assets/Omat Assetit/Scripts/AICar.cs
+++ Assets/Omat Assetit/Scripts/AICar.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class AICar : MonoBehaviour
 {
     public Transform[] waypoints;
+    private int currentWaypointIndex = 0;
     public float speed = 10f;
+    public float rotationSpeed = 5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,6 +16,18 @@ public class AICar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Transform target = waypoints[currentWaypointIndex];
+        Vector3 targetXZ = new Vector3(target.position.x, transform.position.y, target.position.z);
+        Vector3 direction = (targetXZ - transform.position).normalized;
+
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
+
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, targetXZ) < 2f)
+        {
+            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+        }
     }
 }
