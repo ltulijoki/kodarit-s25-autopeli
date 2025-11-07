1d520a186fc84e4b905d24d8c891e49691c02cc1
Leevi Tulijoki <leevi.tulijoki@outlook.com>
2025-11-07 18:19:03 +0200

    vain yksi voittaja

--- Assets/Omat Assetit/Scripts/Referee.cs
+++ Assets/Omat Assetit/Scripts/Referee.cs
@@ -2,10 +2,29 @@ using UnityEngine;
 
 public class Referee : MonoBehaviour
 {
+    private bool winnerDeclared = false;
+
     void OnTriggerEnter(Collider car)
     {
         CarIdentify id = car.GetComponent<CarIdentify>();
         string winnerName = id.displayName;
-        Debug.Log($"Winner: {winnerName}");
+
+        if (id.kind == CarKind.Player)
+        {
+            PlayerLoopCheck validator = car.GetComponent<PlayerLoopCheck>();
+            if (validator == null) return;
+
+            if (!validator.AllVisitedThisLap)
+            {
+                Debug.Log("Pelaaja ylitti maalin, mutta kaikkia checkpointteja ei ole käyty -> ei voittoa");
+                return;
+            }
+        }
+
+        if (!winnerDeclared)
+        {
+            Debug.Log($"Winner: {winnerName}");
+            winnerDeclared = true;
+        }
     }
 }
