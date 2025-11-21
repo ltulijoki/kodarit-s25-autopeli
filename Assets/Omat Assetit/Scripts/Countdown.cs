6f747b56a7b38a6526ea4e996ca611e8f2304640
Leevi Tulijoki <leevi.tulijoki@outlook.com>
2025-11-21 18:18:29 +0200

    tehty countdown

--- Assets/Omat Assetit/Scripts/Countdown.cs
+++ Assets/Omat Assetit/Scripts/Countdown.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 using TMPro;
 
 public class Countdown : MonoBehaviour
@@ -6,4 +7,19 @@ public class Countdown : MonoBehaviour
     public TMP_Text uiText;
     public int countDownFrom = 3;
     public float stepSeconds = 1f;
+
+    IEnumerator Start()
+    {
+        for (int i = countDownFrom; i  > 0; i--)
+        {
+            uiText.text = i.ToString();
+            yield return new WaitForSeconds(stepSeconds);
+        }
+
+        uiText.text = "GO!";
+        yield return new WaitForSeconds(0.5f);
+        uiText.text = "";
+        uiText.gameObject.SetActive(false);
+        GameManager.Instance.Phase = RacePhase.Racing;
+    }
 }
