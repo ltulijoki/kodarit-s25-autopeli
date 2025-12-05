ea7eb6562d6f85705b68bb685b0e10b9760c9262
Leevi Tulijoki <leevi.tulijoki@outlook.com>
2025-12-05 18:15:37 +0200

    lisätty ääniä

--- Assets/Omat Assetit/Scripts/Countdown.cs
+++ Assets/Omat Assetit/Scripts/Countdown.cs
@@ -7,16 +7,21 @@ public class Countdown : MonoBehaviour
     public TMP_Text uiText;
     public int countDownFrom = 3;
     public float stepSeconds = 1f;
+    public AudioClip pling;
+    public AudioClip goSFX;
 
     IEnumerator Start()
     {
+        yield return new WaitForSeconds(1);
         for (int i = countDownFrom; i  > 0; i--)
         {
             uiText.text = i.ToString();
+            AudioManager.Instance.PlaySFX(pling);
             yield return new WaitForSeconds(stepSeconds);
         }
 
         uiText.text = "GO!";
+        AudioManager.Instance.PlaySFX(goSFX);
         yield return new WaitForSeconds(0.5f);
         uiText.text = "";
         uiText.gameObject.SetActive(false);
--- Assets/Omat Assetit/Scripts/Player.cs
+++ Assets/Omat Assetit/Scripts/Player.cs
@@ -4,6 +4,7 @@ public class Player : MonoBehaviour
 {
     public float speed = 10f;
     public float turnSpeed = 50f;
+    public AudioClip engineSFX;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,9 +19,13 @@ public class Player : MonoBehaviour
 
         float move = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         float turn = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
-        // Debug.Log(move);
 
         transform.Translate(Vector3.forward * move);
         transform.Rotate(Vector3.up * turn);
+
+        if (move != 0 || turn != 0)
+        {
+            AudioManager.Instance.PlaySFX(engineSFX);
+        }
     }
 }
